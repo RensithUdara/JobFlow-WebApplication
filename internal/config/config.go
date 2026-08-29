53900545d36108e40b0a712f76ef0398c88f9377
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-29 16:52:51 +0530

    refactor: standardize formatting in Config struct and Load function

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,31 +8,31 @@ import (
 )
 
 type Config struct {
-	Env              string
-	ServerPort       string
-	DatabaseURL      string
-	RedisURL         string
-	JWTSecret        string
-	AllowedOrigin    string
-	WorkerID         string
-	WorkerQueues     []string
-	WorkerCount      int
-	JobTimeout       time.Duration
+	Env               string
+	ServerPort        string
+	DatabaseURL       string
+	RedisURL          string
+	JWTSecret         string
+	AllowedOrigin     string
+	WorkerID          string
+	WorkerQueues      []string
+	WorkerCount       int
+	JobTimeout        time.Duration
 	SchedulerInterval time.Duration
 }
 
 func Load() Config {
 	return Config{
-		Env:              get("APP_ENV", "development"),
-		ServerPort:       get("SERVER_PORT", "8080"),
-		DatabaseURL:      get("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/jobflow?sslmode=disable"),
-		RedisURL:         get("REDIS_URL", "redis://localhost:6379/0"),
-		JWTSecret:        get("JWT_SECRET", "dev-secret-change-me"),
-		AllowedOrigin:    get("ALLOWED_ORIGIN", "http://localhost:5173"),
-		WorkerID:         get("WORKER_ID", ""),
-		WorkerQueues:     split(get("WORKER_QUEUES", "emails,images,webhooks,reports,default")),
-		WorkerCount:      getInt("WORKER_COUNT", 4),
-		JobTimeout:       time.Duration(getInt("JOB_TIMEOUT_SECONDS", 30)) * time.Second,
+		Env:               get("APP_ENV", "development"),
+		ServerPort:        get("SERVER_PORT", "8080"),
+		DatabaseURL:       get("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/jobflow?sslmode=disable"),
+		RedisURL:          get("REDIS_URL", "redis://localhost:6379/0"),
+		JWTSecret:         get("JWT_SECRET", "dev-secret-change-me"),
+		AllowedOrigin:     get("ALLOWED_ORIGIN", "http://localhost:5173"),
+		WorkerID:          get("WORKER_ID", ""),
+		WorkerQueues:      split(get("WORKER_QUEUES", "emails,images,webhooks,reports,default")),
+		WorkerCount:       getInt("WORKER_COUNT", 4),
+		JobTimeout:        time.Duration(getInt("JOB_TIMEOUT_SECONDS", 30)) * time.Second,
 		SchedulerInterval: time.Duration(getInt("SCHEDULER_INTERVAL_SECONDS", 5)) * time.Second,
 	}
 }
