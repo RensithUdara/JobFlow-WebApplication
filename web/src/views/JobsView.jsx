c52a0f57ca9cf93321098b86abaa41f56af02e60
RensithUdara <rensithudaragonalagoda@gmail.com>
2026-08-29 17:41:40 +0530

    add: enhance JobsView to include allFilteredJobs and pageCount in JobTable

--- web/src/views/JobsView.jsx
+++ web/src/views/JobsView.jsx
@@ -5,7 +5,7 @@ import { JobTable } from "../components/JobTable.jsx";
 import { useJobFilters } from "./useJobFilters.js";
 
 export function JobsView(props) {
-  const { filteredJobs, filters, setFilters } = useJobFilters(props.jobs);
+  const { filteredJobs, allFilteredJobs, filters, setFilters, pageCount } = useJobFilters(props.jobs);
 
   return (
     <div className="jobs-layout">
@@ -13,6 +13,7 @@ export function JobsView(props) {
       <div className="content-grid">
         <JobTable
           jobs={filteredJobs}
+          allJobs={allFilteredJobs}
           selectedJob={props.selectedJob}
           onSelect={props.setSelectedJob}
           onRetry={props.retryJob}
@@ -20,6 +21,7 @@ export function JobsView(props) {
           filters={filters}
           onFiltersChange={setFilters}
           onExport={props.exportJobs}
+          pageCount={pageCount}
         />
         <JobDetails job={props.selectedJob || filteredJobs[0]} />
       </div>
